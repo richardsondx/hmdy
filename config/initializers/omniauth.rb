226e347d796d3ac6987abd3b4465d883805696a4
RichardsonDx <richardsondx@gmail.com>
2012-04-30 03:35:11 -0400

    fixing fb login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '109989265800865','744992dd6f454af379aec458a0f86e83',
-  :scope => 'email, user_relationships, user_status, user_about_me, publish_actions', :display => 'popup'
+  provider :facebook, '109989265800865','744992dd6f454af379aec458a0f86e83'
 end
