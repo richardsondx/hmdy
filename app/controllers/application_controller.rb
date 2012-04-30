5b545444968c576da73118857daf67d8885909bb
RichardsonDx <richardsondx@gmail.com>
2012-04-30 07:23:26 -0400

    no rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   
-  rescue_from Timeout::Error, with: :handle_timeout
-
-    def handle_timeout(exception)
-      # handle timeout error
-    end
-  
-  
   private
 
   def current_user
