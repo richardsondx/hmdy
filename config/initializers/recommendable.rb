38914339dadb2f4e1497398f701d199f7e664565
RichardsonDx <richardsondx@gmail.com>
2012-04-30 04:58:02 -0400

    fixing host attem p1

--- config/initializers/recommendable.rb
+++ config/initializers/recommendable.rb
@@ -5,7 +5,7 @@ require "resque-loner"
 # Recommendable requires a connection to a running redis-server. Either create
 # a new instance based on a host/port or UNIX socket, or pass in an existing
 # Redis client instance.
-Recommendable.redis = Redis.new(:host => "localhost ", :port => 6379)
+Recommendable.redis = Redis.new(:host => "localhost", :port => 6379)
 
 # Connect to Redis via a UNIX socket instead
 # Recommendable.redis = Redis.new(:sock => "")
