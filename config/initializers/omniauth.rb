5a5c9f383403ed40e29c600cbcc8ab24e9ed6ed9
RichardsonDx <richardsondx@gmail.com>
2012-04-30 03:12:09 -0400

    phase 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '187370434717652','8726a8623628d9497933189cbba3b577',
+  provider :facebook, '109989265800865','744992dd6f454af379aec458a0f86e83',
   :scope => 'email, user_relationships, user_status, user_about_me, publish_actions', :display => 'popup'
 end
