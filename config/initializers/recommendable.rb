43dad41eca28aca8533a6d0d99866dae6fa7403d
RichardsonDx <richardsondx@gmail.com>
2012-04-30 05:46:43 -0400

    my little update to recommednable

--- config/initializers/recommendable.rb
+++ config/initializers/recommendable.rb
@@ -5,14 +5,8 @@ require "resque-loner"
 # Recommendable requires a connection to a running redis-server. Either create
 # a new instance based on a host/port or UNIX socket, or pass in an existing
 # Redis client instance.
+Recommendable.redis = Redis.new(:host => "guppy.redistogo.com", :port => 6789, :username => "redistogo", :password "23f1313d9c311dc967021e31295f3758")
 
-Resque.redis = REDIS
-
-ENV["REDISTOGO_URL"] ||=  "redis://redistogo:23f1313d9c311dc967021e31295f3758@guppy.redistogo.com:9037/"
-
-uri = URI.parse(ENV["REDISTOGO_URL"])
-
-Recommendable.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
 # Connect to Redis via a UNIX socket instead
 # Recommendable.redis = Redis.new(:sock => "")
