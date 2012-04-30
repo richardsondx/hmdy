00409bfc793a3e4a68702d3bc174111bac0130a7
RichardsonDx <richardsondx@gmail.com>
2012-04-30 05:39:29 -0400

    update

--- config/initializers/recommendable.rb
+++ config/initializers/recommendable.rb
@@ -7,7 +7,9 @@ require "resque-loner"
 # Redis client instance.
 
 ENV["REDISTOGO_URL"] ||=  "redis://redistogo:23f1313d9c311dc967021e31295f3758@guppy.redistogo.com:9037/"
+
 uri = URI.parse(ENV["REDISTOGO_URL"])
+
 Recommendable.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
 # Connect to Redis via a UNIX socket instead
